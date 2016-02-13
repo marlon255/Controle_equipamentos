43b1fd2df54096b98c413b9af08ee9b14564c22f
marlon255 <marlon.0255@gmail.com>
2016-02-13 15:11:13 -0300

    Adicionando mudança.

--- tipo_equi.php
+++ tipo_equi.php
@@ -36,23 +36,27 @@
 				<li>Ações</li>
 			</ul>
 		</div>
+<?php
+	if(isset($_POST['salvar_tipo_equi'.$exibir['id']])){
+				$tipo_new = $_POST['seacher_tipo_equi'.$exibir['id']];
+				$sigla_new = $_POST['seacher_tipo_sigla'.$exibir['id']];
+				echo ;
+				$salvar = mysqli_query($conexao, sprintf("UPDATE tipo_equipamento SET tipo_equip='$tipo_new', sigla_equip='$sigla_new'")) or die(mysqli_error());
+	}
+?>
 <?php 
 if($total > 0){
 do{ ?>	
 	<?php include ("php/javascript_editar.php") ?>
 	<div id="change_tipo_equi">
-			<form method="POST"><input type='text' id="seacher_tipo_equi<?=$exibir['id']?>" name="seacher_tipo_equi<?=$exibir['id']?>" class="seacher_tipo_equi" style='border-left: 1px solid;' disabled value="<?php echo $exibir['tipo_equip']; ?>"></form>
-			<form method="POST"><input type='text' id="seacher_tipo_sigla<?=$exibir['id']?>" name="seacher_tipo_sigla<?=$exibir['id']?>" class="seacher_tipo_sigla" disabled value="<?php echo $exibir['sigla_equip']; ?>"></form>
-			<form method="POST"><input type="button" id="editar_tipo_equi<?=$exibir['id']?>" name="editar_tipo_equi<?=$exibir['id']?>" class="teste" value='Editar'></form>
-			<form method="POST"><input type="submit" id="salvar_tipo_equi<?=$exibir['id']?>" name="salvar_tipo_equi<?=$exibir['id']?>" class='teste' value='Salvar'></form>
-			<form method="POST"><input type="submit" id="deletar_tipo_equi<?=$exibir['id']?>" name="deletar_tipo_equi<?=$exibir['id']?>" class='teste' value='Deletar'></form>
-		</div>
-<?php
-	if(isset($_POST['salvar_tipo_equi'.$exibir["id"].''])){
-		$procv = $_POST['seacher_tipo_equi'.$exibir['id'].''];
-		echo $procv;
-	}
-?>
+		<form method="POST" id="teste<?=$exibir['id']?>">
+			<input type='text' id="seacher_tipo_equi<?=$exibir['id']?>" name="seacher_tipo_equi<?=$exibir['id']?>" class="seacher_tipo_equi" style='border-left: 1px solid;' disabled value="<?php echo $exibir['tipo_equip']; ?>">
+			<input type='text' id="seacher_tipo_sigla<?=$exibir['id']?>" name="seacher_tipo_sigla<?=$exibir['id']?>" class="seacher_tipo_sigla" disabled value="<?php echo $exibir['sigla_equip']; ?>">
+			<input type="button" id="editar_tipo_equi<?=$exibir['id']?>" name="editar_tipo_equi<?=$exibir['id']?>" class="teste" value='Editar'>
+			<input type="submit" id="salvar_tipo_equi<?=$exibir['id']?>" name="salvar_tipo_equi<?=$exibir['id']?>" class='teste' value='Salvar'>
+			<input type="submit" id="deletar_tipo_equi<?=$exibir['id']?>" name="deletar_tipo_equi<?=$exibir['id']?>" class='teste' value='Deletar'>
+		</form>
+	</div>
 	<?php	}while($exibir = mysqli_fetch_assoc($selecionar));
 	}
 ?>
