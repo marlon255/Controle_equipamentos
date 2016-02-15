39b8fc966c2e7a7eba5ecec09cb945bb399ec274
marlon255 <marlon.0255@gmail.com>
2016-02-15 15:56:06 -0300

    Update banco.sql

--- DOCS/banco.sql
+++ DOCS/banco.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 12-Fev-2016 às 18:18
+-- Generation Time: 15-Fev-2016 às 18:55
 -- Versão do servidor: 5.6.20-log
 -- PHP Version: 5.4.31
 
@@ -23,32 +23,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tipo_equipamento`
+-- Estrutura da tabela `cadastro_proprietario`
 --
 
-CREATE TABLE IF NOT EXISTS `tipo_equipamento` (
+CREATE TABLE IF NOT EXISTS `cadastro_proprietario` (
 `id` int(10) NOT NULL,
-  `tipo_equip` varchar(25) NOT NULL,
-  `sigla_equip` varchar(3) NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `raz_soc` varchar(48) NOT NULL,
+  `nome_fant` varchar(32) NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
+  `insc_estad` varchar(15) NOT NULL,
+  `insc_munic` varchar(15) NOT NULL,
+  `cep` varchar(9) NOT NULL,
+  `estado` varchar(25) NOT NULL,
+  `cidade` varchar(25) NOT NULL,
+  `bairro` varchar(25) NOT NULL,
+  `endereco` varchar(25) NOT NULL,
+  `comple` varchar(25) NOT NULL,
+  `telefone` varchar(13) NOT NULL,
+  `email` varchar(48) NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Extraindo dados da tabela `tipo_equipamento`
+-- Extraindo dados da tabela `cadastro_proprietario`
 --
 
-INSERT INTO `tipo_equipamento` (`id`, `tipo_equip`, `sigla_equip`) VALUES
-(1, 'a', 'a'),
-(2, '', ''),
-(3, 'dd', 'dd');
+INSERT INTO `cadastro_proprietario` (`id`, `raz_soc`, `nome_fant`, `cnpj`, `insc_estad`, `insc_munic`, `cep`, `estado`, `cidade`, `bairro`, `endereco`, `comple`, `telefone`, `email`) VALUES
+(1, 'a', 'a', '11.111.111/1111-11', '111.111.111.111', '111111111111111', '11111-111', 'a', 'a', 'a', 'a', 'a', '33 33333-3333', 'a@a.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tipo_equipamento`
+-- Indexes for table `cadastro_proprietario`
 --
-ALTER TABLE `tipo_equipamento`
+ALTER TABLE `cadastro_proprietario`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -56,10 +65,10 @@ ALTER TABLE `tipo_equipamento`
 --
 
 --
--- AUTO_INCREMENT for table `tipo_equipamento`
+-- AUTO_INCREMENT for table `cadastro_proprietario`
 --
-ALTER TABLE `tipo_equipamento`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `cadastro_proprietario`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
